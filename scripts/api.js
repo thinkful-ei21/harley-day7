6239f5ed8699b1877a04f354c9fd3f135097452a
harleyjj <harleyyesm@gmail.com>
2018-05-15 14:10:31 -0700

    hardcoded api key

--- scripts/api.js
+++ scripts/api.js
@@ -3,19 +3,17 @@
 
 // eslint-disable-next-line no-unused-vars
 const api = (function() {
-  const API_KEY = 'AIzaSyC-_OFqHR01cjpTSjpa1K0G9eIuwwKlDMk';
   const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
   const fetchVideos = function(searchTerm, callback) {
     const query = {
       part: 'snippet',
-      key: this.API_KEY,
+      key: 'AIzaSyC-_OFqHR01cjpTSjpa1K0G9eIuwwKlDMk',
       q: `${searchTerm}`,
     };
     $.getJSON(this.BASE_URL, query, callback);
   };
 
   return {
-    API_KEY,
     BASE_URL,
     fetchVideos
   };
